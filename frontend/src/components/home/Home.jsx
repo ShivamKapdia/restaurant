daa1ce3960e2883f6c72f8294b308bb6c909960b
Shivam Kapdia <shivamkapdia28@gmail.com>
2025-08-05 00:34:47 +0530

    ok

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Navbar from "../navbar/Navbar.jsx";
+import img from "../../assets/bg-image2.png";
 
 const Home = () => {
-  return <h1>this is shivam restaurant</h1>;
+  return (
+    <>
+      <Navbar />
+      <div style={{ margin: 0, padding: 0 }}>
+        <img
+          src={img}
+          alt="Shivam restaurant"
+          style={{
+            margin: 0,
+            padding: 0,
+            display: "block",
+            width: "100%",
+            maxWidth: "100vw",
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Home;
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Navbar.css"; // Create this CSS file for custom styles
+
+const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{ backgroundColor: "#F5F5E0" }}
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Logo
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Menu
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="contact-box d-none d-lg-block">
+          <span>📞 +91 98765 43210</span>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
