05b7a5589722754b2e9c9218a31a8940dd02fcef
Shivam Kapdia <shivamkapdia28@gmail.com>
2025-08-19 15:13:05 +0530

    ok

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -1,5 +1,25 @@
+import menu1 from "../../assets/menu-1.png";
+import menu2 from "../../assets/menu-2.png";
+import menu3 from "../../assets/menu-3.png";
+import "./Menu.css";
+
 const Menu = () => {
-  return <p>Menu</p>;
+  return (
+    <div className="menu-container">
+      <h1 className="menu-heading">Our Menu</h1>
+      <div className="menu-sections">
+        <div className="menu-section">
+          <img src={menu1} alt="Menu Section 1" />
+        </div>
+        <div className="menu-section">
+          <img src={menu2} alt="Menu Section 2" />
+        </div>
+        <div className="menu-section">
+          <img src={menu3} alt="Menu Section 3" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
